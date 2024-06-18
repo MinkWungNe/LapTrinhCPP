560aedcbf81ee2b88efbde6ebd6a3276c6a3db40
MinkWungNe <leluongminhquan@gmail.com>
2024-06-18 11:37:52 +0700

    update day 4

---
+++ BaiTapThucHanh/Ngay4/Bai_3_BAITONGHOP.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+#include "employeeType.h"
+using namespace std;
---
+++ BaiTapThucHanh/Ngay4/Bai_4_Test_fullTImeEmployee.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "fullTimeEmployee.h"
+using namespace std;
+
+int main()
+{
+    fullTimeEmployee newEMP("Nguyen","Van Dung", 75, 56000, 5700);
+    newEMP.print();
+    cout << endl;
+}
