a3272eeabd46684b3d7dd3d4a175c6f3cdfe05a8
MinkWungNe <leluongminhquan@gmail.com>
2024-06-25 08:38:44 +0700

    fullTimeEmployee

--- BaiTapThucHanh/Ngay4/Bai_4_Test_FandPTImeEmployee.cpp
+++ BaiTapThucHanh/Ngay4/Bai_4_Test_FandPTImeEmployee.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "fullTimeEmployee.h"
+#include "partTimeEmployee.h"
 using namespace std;
 
 int main()
 {
     fullTimeEmployee newEMP("Nguyen","Van Dung", 75, 56000, 5700);
+    partTimeEmployee tempEMP("Bill", "Neilson", 275, 15.50, 57);
     newEMP.print();
+    cout << endl;
+    tempEMP.print();
     cout << endl;
 }
